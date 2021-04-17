be4a03fde90c15dd11b5941f9f90b6699f383497
Mahin Rashid <mahinr369@gmail.com>
2021-04-17 07:15:32 +0600

    temp

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const user_db = require('../models/user_db');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: './static/Images/profile_pic',
+    filename: function(req, file, callback){
+        callback(null, `${req.session.active_id}.jpg`);
+    }
+
+});
+
+const upload = multer({storage: storage}).single('profile_pic');
+
 var router = express.Router();
 
 router.get('/profile', (req, res)=>{
@@ -17,5 +29,17 @@ router.post('/profile', (req, res)=>{
     })
     res.redirect('/index');
 })
+
+router.post('/profile_pic', (req, res)=>{
+    upload(req, res, (err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(req.file);;
+        }
+    })
+})
+
 
 module.exports = router;
