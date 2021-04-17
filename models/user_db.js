ff079ca4b2e85e1b16c65eb23a7f06d19e6a8133
abdarker <darkerab@gmail.com>
2021-04-17 17:54:30 +0600

    gave entire website a whole new look

--- models/user_db.js
+++ models/user_db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
-    password: 'abcd',
+    password: '',
     user:'root',
     database:'ebooking'
 })
